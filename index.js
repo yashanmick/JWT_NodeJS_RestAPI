f1993d3011193bbc371a2637545aba8cca7d44a6
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-08 20:13:14 +0530

    middleware created

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//Import routes
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 //Connect to DB
@@ -13,11 +16,10 @@ mongoose.connect(process.env.DB_CONNECTION,
     }
 );
 
+//Middleware
+app.use(express.json());
 
-//Import routes
-const authRoute = require('./routes/auth');
-
-//Middlewares
+//Route Middlewares
 app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server is running'));
