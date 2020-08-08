0e0de4e62cd9aa04261c82bcb6690a8ac4366319
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-08 21:11:26 +0530

    user registration validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const { registerValidation } = require('../validation');
+
+
+
+
 
 router.post('/register', async (req, res) => {
+
+    //Validate the data before we make a user
+    const { error } = registerValidation(req.body);
+    // res.send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message)
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
