887fbc915058bd7f019e7a95d0935367c60f0ead
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-09 00:17:15 +0530

    fetch data using jwt token

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const verify = require('./verifyToken');
 
-router.get('/', (req, res) => {
-    res.json({ posts: { title: 'my first post', description: 'desc' } });
+router.get('/', verify, (req, res) => {
+    // res.json({ posts: { title: 'my first post', description: 'desc' } });
+    res.send(req.user);
+    User.findbyOne({ _id: req.user })
 });
 
 
