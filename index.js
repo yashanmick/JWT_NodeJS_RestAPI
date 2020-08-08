e4b76a09f6db43be4c4b774686f860bf37f2268e
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-08 19:53:09 +0530

    encryption

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const { Router } = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connect to DB');
+    }
+);
+
 
 //Import routes
 const authRoute = require('./routes/auth');
