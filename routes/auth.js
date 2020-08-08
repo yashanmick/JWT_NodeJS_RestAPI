ccd6149d533dfe1be64c062f0a981178142d7be9
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-08 21:15:45 +0530

    check if the user already in the database

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,13 @@ router.post('/register', async (req, res) => {
     //Validate the data before we make a user
     const { error } = registerValidation(req.body);
     // res.send(error.details[0].message);
-    if (error) return res.status(400).send(error.details[0].message)
+    if (error) return res.status(400).send(error.details[0].message);
 
+    //Check if the user already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already exists');
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
